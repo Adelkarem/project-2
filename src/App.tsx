4f42c0623eadae34d3a8422e99d59eba0a67d59a
Adelkarem <adelkarem376@yahoo.com>
2024-02-12 19:06:00 -0800

    error message toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Model from "./components/Ui/Model";
 import { formInputsList, productList } from "./data";
 import Button from "./components/Ui/Button";
 import Input from "./components/Ui/Input";
 import { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import ErrorMassage from "./components/ErrorMassage";
 
 const App = () => {
 
-const [product , setproduct]= useState <IProduct>({
-  title:'',
-  description:'',
-  imageURL:'',
-  price:'',
-  colors:[],
-  category:{
-    name:'',
+  const defaultProductObj = {
+    title:'',
+    description:'',
     imageURL:'',
+    price:'',
+    colors:[],
+    category:{
+      name:'',
+      imageURL:'',
+    }
   }
-})
+const [product , setproduct]= useState <IProduct>(defaultProductObj)
+const [errors ,setErrors]=useState({title:'',
+description:'',
+imageURL:'',
+price:''})
 
 
   /* state*/
@@ -39,23 +46,51 @@ setproduct({
   ...product,
   [name]:value,
 })
+setErrors({
+  ...errors,
+  [name]:"",
+})
 }
 
+
+const submitHandler=(event: FormEvent<HTMLFormElement>): void=> {
+event.preventDefault();
+const {title ,description,price,imageURL}= product
+const errors = productValidation({title , description, price,imageURL})
+
+console.log(errors);
+const hasErrorMsg =Object.values(errors).some(value => value==="")&& Object.values(errors).every(value=>value==="")
+if(!hasErrorMsg){
+  setErrors(errors)
+  return;
+}
+console.log("SEND THIS PRODUCT TO OUR SERVER")
+}
+
+
+
+const onCancel =()=>{
+  setproduct(defaultProductObj)
+  closeModal()
+}
     /* render*/
   const renderProductList = productList.map(product => (
     <ProductCard key={product.id} product={product} />
   ));
 
   const renderFormInputList = formInputsList.map(input => (
-    <div className="flex flex-col">
+    <div className="flex flex-col" key={input.id}>
       <label
         htmlFor={input.id}
         className="mb-[px] text-sm font-medium text-gray-700">
         {input.label}
       </label>
       <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={onChangeHandler} />
+    <ErrorMassage msg={errors[input.name]}/>
     </div>
   ));
+
+
   return (
     <main className="container">
       <Button className="bg-red-700 hover:bg-indigo-800" onClick={openModal}>
@@ -67,12 +102,12 @@ setproduct({
       </div>
 
       <Model isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <div className="space-y-2">
+        <div className="space-y-2"> 
           {renderFormInputList}
 
-          <form className="flex items-center space-x-3  ">
+          <form className="flex items-center space-x-3  "  onSubmit={submitHandler}>
             <Button className="bg-red-700 hover:bg-indigo-800">SNBMIT</Button>
-            <Button className="bg-gray-400 hover:bg-gray-500">Cancle</Button>
+            <Button className="bg-gray-400 hover:bg-gray-500" onClick={onCancel}>Cancle</Button>
           </form>
         </div>
       </Model>
---
+++ src/validation/index.ts
@@ -0,0 +1,42 @@
+export const productValidation = (product: {
+    title: string;
+    description: string;
+    imageURL: string;
+    price: string;
+}): {
+    title: string;
+    description: string;
+    imageURL: string;
+    price: string;
+} => {
+    const errors: {
+        title: string;
+        description: string;
+        imageURL: string;
+        price: string;
+    } = {
+        title: '',
+        description: '',
+        imageURL: '',
+        price: '',
+    };
+
+const validUrl = /^(ftp|http|https ):\/\/[^."]+$/.test(product.imageURL)
+
+    if (! product.title.trim() || product.title.length < 10 || product.title.length > 80) {
+        errors.title = "Product title must be between 10 and 80 characters";
+    }
+
+    if (! product.description.trim() || product.description.length < 10 || product.description.length > 800) {
+        errors.description = "Product description must be between 10 and 80 characters";
+    }
+
+    if (!product.imageURL.trim() || !validUrl){
+        errors.imageURL="vaild image URL is required";
+    }
+
+    if(!product.price.trim() || isNaN(Number(product.price))){
+        errors.price="price must be a number";
+    }
+    return errors;
+};
